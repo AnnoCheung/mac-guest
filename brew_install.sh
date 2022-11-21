19f33e4f82654a63755ddd71cb2392cbf049bdc8
Anno <81018792+AnnoCheung@users.noreply.github.com>
2022-11-22 02:52:15 +0800

    Add files via upload

---
+++ brew_install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Homebrew/brew homebrew
+
+eval "$(homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+
+echo 'eval "$(homebrew/bin/brew shellenv)"' >> ~/.zshrc

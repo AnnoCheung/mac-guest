6ca7a690e0755b6db34928c57b8de291bcad0407
Anno <81018792+AnnoCheung@users.noreply.github.com>
2023-01-22 10:19:43 +0800

    added brew shell completion

--- brew_install.sh
+++ brew_install.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 cd ~
 git clone https://github.com/Homebrew/brew homebrew
 
@@ -6,3 +8,5 @@ brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 
 echo 'eval "$(homebrew/bin/brew shellenv)"' >> ~/.zshrc
+#Create brew shell completion in ~/.zshrc
+echo '#Brew Shell Completion'$'\n''if type brew &>/dev/null'$'\n''then'$'\n''  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"'$'\n'$'\n''  autoload -Uz compinit'$'\n''  compinit'$'\n''fi' >> ~/.zshrc

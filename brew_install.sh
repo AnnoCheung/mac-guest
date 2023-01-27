8950e60d0df4cee9f1b46cb24bcc28b2923afbbd
Anno <81018792+AnnoCheung@users.noreply.github.com>
2023-01-27 19:10:59 +0800

    changed default --appdir

--- brew_install.sh
+++ brew_install.sh
@@ -10,3 +10,5 @@ chmod -R go-w "$(brew --prefix)/share/zsh"
 echo 'eval "$(homebrew/bin/brew shellenv)"' >> ~/.zshrc
 #Create brew shell completion in ~/.zshrc
 echo '#Brew Shell Completion'$'\n''if type brew &>/dev/null'$'\n''then'$'\n''  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"'$'\n'$'\n''  autoload -Uz compinit'$'\n''  compinit'$'\n''fi' >> ~/.zshrc
+#Change default appdir as Guest users can't access /Applications folder
+export HOMEBREW_CASK_OPTS="--appdir=~/Applications" # removed if needed --caskroom=/usr/local/Caskroom

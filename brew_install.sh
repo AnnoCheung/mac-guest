6c390e6b01f39fb7985912d89263448b504851fd
Anno <81018792+AnnoCheung@users.noreply.github.com>
2023-01-27 19:14:58 +0800

    Update brew_install.sh

--- brew_install.sh
+++ brew_install.sh
@@ -12,3 +12,6 @@ echo 'eval "$(homebrew/bin/brew shellenv)"' >> ~/.zshrc
 echo '#Brew Shell Completion'$'\n''if type brew &>/dev/null'$'\n''then'$'\n''  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"'$'\n'$'\n''  autoload -Uz compinit'$'\n''  compinit'$'\n''fi' >> ~/.zshrc
 #Change default appdir as Guest users can't access /Applications folder
 export HOMEBREW_CASK_OPTS="--appdir=~/Applications" # removed if needed --caskroom=/usr/local/Caskroom
+
+#Finish message
+echo "Homebrew installation (untar) is finished, please restart your terminal instance to use homebrew!"

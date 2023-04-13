40fb108b10d500571e2f6b4e66cc191c10bb9b47
Anno <81018792+AnnoCheung@users.noreply.github.com>
2023-04-13 14:11:15 +0800

    Add files via upload

---
+++ Profile_DL.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+fileid="1s04o5Vy1Soev-q8gb89BZsXFR-IUoRQp"
+filename="def.zip"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+
+echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'

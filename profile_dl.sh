a90143c046c30a428de2167a35fc67f72ed2857a
Anno <81018792+AnnoCheung@users.noreply.github.com>
2023-04-18 17:12:14 +0800

    Update and rename Profile_DL.sh to profile_dl.sh

--- profile_dl.sh
+++ profile_dl.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 fileid="1s04o5Vy1Soev-q8gb89BZsXFR-IUoRQp"
-filename="Deafult.zip"
+filename="Default.zip"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Eio '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
 
 #echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'

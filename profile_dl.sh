d8bbc299c85a1d34238a4004da3c67467ce8cd70
Anno <81018792+AnnoCheung@users.noreply.github.com>
2023-04-25 16:45:44 +0800

    Update profile_dl.sh

--- profile_dl.sh
+++ profile_dl.sh
@@ -5,4 +5,6 @@ html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${f
 
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Eio '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
 
+echo -e "mkdir -p $HOME/Library/Application Support/Google/Chrome\n"
+echo -e "unzip -d $HOME/Library/Application Support/Google/Chrome" $filename
 #echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'

1707cc242bc4c5e41b68cc7839818e816cb88002
Anno <81018792+AnnoCheung@users.noreply.github.com>
2024-09-03 12:57:29 +0800

    Update profile_dl.sh
    Changed to new endpoint

--- profile_dl.sh
+++ profile_dl.sh
@@ -3,7 +3,8 @@ fileid="1s04o5Vy1Soev-q8gb89BZsXFR-IUoRQp"
 filename="Default.zip"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Eio '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+#curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Eio '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+curl -L "https://drive.usercontent.google.com/download?id=`echo ${fileid}`&confirm=xxx" -o ${filename}
 
 echo -e "mkdir -p \"$HOME/Library/Application Support/Google/Chrome\""
 echo -e "unzip -d \"$HOME/Library/Application Support/Google/Chrome\"" $filename
